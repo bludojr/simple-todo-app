538ada01f794a63261db2f7187638ff7336ddf40
Ewusi David Abedu <150860194+bludojr@users.noreply.github.com>
2024-10-17 16:50:00 +0000

    Styled  mobile app background

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    {/* <div className="background">
+      <div className="card">
+        <p>12:30</p>
+        <img src="src\images\Menu.svg" alt="Menu" className='scale-50 pl-[10px] '/>
+        <h2 className='red-900'>Simple To-Do</h2>
+      </div>
+    </div> */}
+    </>
+  )
+}
+
+export default Card
