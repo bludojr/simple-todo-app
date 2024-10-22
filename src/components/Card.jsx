4ad3cacad857433fd40d7a440d633f51e631b8da
Ewusi David Abedu <150860194+bludojr@users.noreply.github.com>
2024-10-22 10:34:08 +0000

    Refactored Changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,6 @@
-import React from 'react'
-
 const Card = () => {
   return (
     <>
-    {/* <div className="background">
-      <div className="card">
-        <p>12:30</p>
-        <img src="src\images\Menu.svg" alt="Menu" className='scale-50 pl-[10px] '/>
-        <h2 className='red-900'>Simple To-Do</h2>
-      </div>
-    </div> */}
     </>
   )
 }
